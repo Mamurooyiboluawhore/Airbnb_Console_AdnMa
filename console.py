97d71df78ade3c2b5045a8612c86184aaa2729ee
Adetwelve <niyero10@gmail.com>
2023-06-20 21:44:21 +0000

    Added 'update' functionality to console

--- console.py
+++ console.py
@@ -51,5 +51,57 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print(storage.all()[f"{arg[0]}.{arg[1]}"])
 
+        
+        def do_destroy(self, args):
+            """Deletes an instance based on the class name and id
+            """
+            arg = args.split()
+    
+            if len(arg) == 0:
+                print("** class name missing **")
+            elif arg[0] not in self.__classes:
+                print("** class doesn't exist **")
+            elif len(arg) == 1:
+                print("** instance id missing **")
+            elif f"{arg[0]}.{arg[1]}" not in storage.all():
+                print("** no instance found **")
+            else:
+                del[storage.all()[f"{arg[0]}.{arg[1]}"]]
+                storage.save()
+        
+        def do_all(self, args):
+
+            if not args:
+                print("** class name missing **")
+            elif args not in self.__classes:
+                print("** class doesn't exist **")
+            elif args:
+                objects = [str(v) for k, v in storage.all().items()]
+                print(objects)
+        
+        def do_update(self, args):
+            arg = args.split()
+
+            if not arg:
+                print("** class name missing **")
+            elif arg[0] not in self.__classes:
+                print("** class doesn't exist **")
+            elif not arg[1]:
+                print('** instance id missing **')
+            elif f'{arg[0]}.{arg[1]}' not in storage.all():
+                print("** no instance found **")
+            elif not arg[2]:
+                print("** attribute name missing **")
+            elif not arg[3]:
+                print("** value missing **")
+            else:
+                key = f'{arg[0]}.{arg[1]}'
+                new_obj = storage.all().get(key)
+                value = arg[3][1:-1]
+                setattr(new_obj, arg[2], value)
+                storage.save()
+
+
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()

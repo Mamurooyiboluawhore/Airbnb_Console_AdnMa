3d479197da3c31ae8fc31464e7b2559031cbb826
Almadny <Almadny2u@gmail.com>
2023-06-21 20:23:12 +0000

    Added all the other files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Airbnb_Console_By_Peers
+this is a personal project done by Ahmad, Adeniyi and Mamuro
---
+++ console.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python3
+import cmd
+from models.base_model import BaseModel
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import city
+from models.amenity import Amenity
+from models.review import Review
+
+class HBNBCommand(cmd.Cmd):
+        prompt = "(hbnb) "
+        __classes = {'BaseModel': BaseModel,
+                    'User': User,
+                    'Place': Place,
+                    'State': State,
+                    'City': City,
+                    'Amenity': Amenity,
+                    'Review': Review
+                    }
+
+        def do_quit(self, args):
+            """Quit command to exit the program
+            """
+            return True
+
+        def do_EOF(self, args):
+            """end of file command to exit the program
+	    """
+            return True
+
+        def emptyline(self):
+            """ A command to end last command 
+            """
+            pass
+        def do_create(self, args):
+            """ creates a new instance
+            """
+            if len(args) == 0:
+                print("** class name missing **")
+
+            elif args not in self.__classes:
+                print("** class doesn't exist **")
+
+            else:
+                new_obj = self.__classes[args]()
+                storage.save()
+                print(f"{new_obj.id}")
+
+        def do_show(self, args):
+            """Prints the string representation
+            """
+            arg = args.split()
+    
+            if len(arg) == 0:
+                print("** class name missing **")
+            elif arg[0] not in self.__classes:
+                print("** class doesn't exist **")
+            elif len(arg) == 1:
+                print("** instance id missing **")
+            elif f"{arg[0]}.{arg[1]}" not in storage.all():
+                print("** no instance found **")
+            else:
+                print(storage.all()[f"{arg[0]}.{arg[1]}"])
+
+        
+        def do_destroy(self, args):
+            """Deletes an instance based on the class name and id
+            """
+            arg = args.split()
+    
+            if len(arg) == 0:
+                print("** class name missing **")
+            elif arg[0] not in self.__classes:
+                print("** class doesn't exist **")
+            elif len(arg) == 1:
+                print("** instance id missing **")
+            elif f"{arg[0]}.{arg[1]}" not in storage.all():
+                print("** no instance found **")
+            else:
+                del[storage.all()[f"{arg[0]}.{arg[1]}"]]
+                storage.save()
+        
+        def do_all(self, args):
+
+            if not args:
+                print("** class name missing **")
+            elif args not in self.__classes:
+                print("** class doesn't exist **")
+            elif args:
+                objects = [str(v) for k, v in storage.all().items()]
+                print(objects)
+        
+        def do_update(self, args):
+            arg = args.split()
+
+            if not arg:
+                print("** class name missing **")
+            elif arg[0] not in self.__classes:
+                print("** class doesn't exist **")
+            elif not arg[1]:
+                print('** instance id missing **')
+            elif f'{arg[0]}.{arg[1]}' not in storage.all():
+                print("** no instance found **")
+            elif not arg[2]:
+                print("** attribute name missing **")
+            elif not arg[3]:
+                print("** value missing **")
+            else:
+                key = f'{arg[0]}.{arg[1]}'
+                new_obj = storage.all().get(key)
+                value = arg[3][1:-1]
+                setattr(new_obj, arg[2], value)
+                storage.save()
+
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
